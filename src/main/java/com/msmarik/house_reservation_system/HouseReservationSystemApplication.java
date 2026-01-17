89fae72f101acc78e7d62125425bded258a7d2d9
msmarik <msmarik@primerevenue.com>
2026-01-17 14:39:42 +0100

    Initial commit.

---
+++ src/main/java/com/msmarik/house_reservation_system/HouseReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.msmarik.house_reservation_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msmarik/house_reservation_system/HouseReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msmarik.house_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
